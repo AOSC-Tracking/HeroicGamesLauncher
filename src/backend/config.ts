837ccad2220e2d286cadf9d0297a846e3e897cf7
SkyBird233 <52884766+SkyBird233@users.noreply.github.com>
2026-01-30 15:24:53 +0800

    loongarch64: enable WoW64 by default

--- src/backend/config.ts
+++ src/backend/config.ts
@@ -346,7 +346,7 @@ class GlobalConfigV0 extends GlobalConfig {
       enableMsync: isMac,
       enableWineWayland: false,
       enableHDR: false,
-      enableWoW64: false,
+      enableWoW64: true,
       eacRuntime: isLinux,
       battlEyeRuntime: isLinux,
       framelessWindow: false,
